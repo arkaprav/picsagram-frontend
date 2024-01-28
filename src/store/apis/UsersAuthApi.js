6066969a30bc8586f3b3d568d12157a879ff3af2
arkaprav <chakraarkaprava@gmail.com>
2024-01-28 18:03:45 +0530

    updated url in reducers

--- src/store/apis/UsersAuthApi.js
+++ src/store/apis/UsersAuthApi.js
@@ -4,7 +4,7 @@ import { axiosBaseQuery } from "../customeBaseQuery/axiosBaseQuery";
 const UsersAuthApi = createApi({
     reducerPath: "UsersAuth",
     baseQuery: axiosBaseQuery({
-        baseUrl: "http://localhost:5001/api/users/auth",
+        baseUrl: "https://picsa-gram-backend.vercel.app//api/users/auth",
     }),
     endpoints(builders){
         return {
